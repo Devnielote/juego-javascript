864cac80dcf69bd6a1ff6788bdd1b2383678488b
Devnielote <danielfc_26@hotmail.com>
2023-04-22 10:09:29 -0600

    Inital files

---
+++ game.js
@@ -0,0 +1,27 @@
+const canvas = document.querySelector("#game")
+const game = canvas.getContext('2d');
+
+window.addEventListener('load', startGame);
+
+function startGame() {
+  let canvasSize;
+  if (window.innerHeight > window.innerWidth) {
+    canvasSize = window.innerWidth * 0.8;
+  } else {
+    canvasSize = window.innerHeight * 0.8;
+  }
+
+  let elementsSize = canvasSize / 10;
+
+  canvas.setAttribute('width', canvasSize)
+  canvas.setAttribute('height', canvasSize)
+  // game.fillRect(100, 50, 100, 50, );
+  // game.font = '25px sans-serif';
+  // game.fillStyle = 'purple';
+  // game.fillText(elementsSize, 100, 100)
+  game.font = elementsSize + 'px Verdana'
+  game.textAlign = 'end';
+  for (let i = 1; i <= 10; i++) {
+    game.fillText(emojis.X, elementsSize * i, elementsSize)
+  }
+}
